474ec3ebaddc96523a7e6333d981a62e2364b1de
robin nganga smith <saintofcookies.2021@gmail.com>
2024-01-18 12:13:11 +0300

    correcting mistakes for seed.py to work

--- app/app.py
+++ app/app.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
+import os
+# from flask_restful import Api, Resource
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
+
+file_path = os.path.abspath(os.getcwd()) + '/db/app.db'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+# api = API(app)
+@app.route('/heroes', methods = ['GET'])
+def get_heroes():
 
-@app.route('/')
-def home():
-    return ''
-
+@app.route('/heroes/<int:hero_id>', methods=['GET'])
+def get_hero(hero_id):
+    hero = next((hero for hero in heroes if hero["id"] == hero_id), None)
+    if hero is None:
+        return jsonify({"error": "Hero not found"}), 404
+    return jsonify(hero)
 
 if __name__ == '__main__':
     app.run(port=5555)
--- app/models.py
+++ app/models.py
@@ -1,19 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     hero_powers = db.relationship('HeroPower', backref='hero', lazy=True)
+    
 
 class Power(db.Model):
+    __tablename__ = 'powers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=False)
-
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    hero_powers = db.relationship('HeroPower', backref='power', lazy=True)
     @validates('description')
     def validate_description(self, key, value):
         if len(value) < 20:
@@ -21,13 +29,19 @@ class Power(db.Model):
         return value
 
 class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
     id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
     strength = db.Column(db.Enum('Strong', 'Weak', 'Average'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     @validates('strength')
     def validate_strength(self, key, value):
         if value not in ['Strong', 'Weak', 'Average']:
             raise ValueError("Invalid strength value. Must be one of 'Strong', 'Weak', or 'Average'.")
         return value
+
+
+
